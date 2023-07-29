c5919967f604689be5e0903016a8790c7224bbc9
mikeqiu2011 <mikeqiu2011@163.com>
2023-07-29 14:19:04 +0800

    get a store

--- app.py
+++ app.py
@@ -37,13 +37,22 @@ def create_store():
 def add_item(name):
     store = [store for store in stores if store['name'] == name]
     if not store:
-        return {'response': 'store does not exist'}, 400
+        return {'message': 'store does not exist'}, 404
 
     store = store[0]
     item = request.get_json()
     store['items'].append(item)
 
     return store, 201
+
+
+@app.get('/store/<string:name>')
+def get_store(name):
+    store = [store for store in stores if store['name'] == name]
+    if not store:
+        return {'message': 'store does not exist'}, 404
+
+    return store[0], 200
 
 
 if __name__ == '__main__':
