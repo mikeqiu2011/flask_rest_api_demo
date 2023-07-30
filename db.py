b31c262cb2d0fd2303685b826437c09338e69b7a
mikeqiu2011 <mikeqiu2011@163.com>
2023-07-30 10:23:17 +0800

    create sqlalchemy model

--- db.py
+++ db.py
@@ -1,15 +1,3 @@
-stores = {
-    "8c7119f68c764356b4fbda841cbb7974": {
-        "name": "cosco",
-        "id": "8c7119f68c764356b4fbda841cbb7974"
-    }
-}
+from flask_sqlalchemy import SQLAlchemy
 
-items = {
-    "c561ab047ca44ea7847b2e092226101d": {
-        "id": "c561ab047ca44ea7847b2e092226101d",
-        "name": "milk",
-        "price": 10,
-        "store_id": "8c7119f68c764356b4fbda841cbb7974"
-    }
-}
+db = SQLAlchemy()
